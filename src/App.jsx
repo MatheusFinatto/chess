d8aa155c59b3b26d70f98e4237d84a8a53983345
MatheusFinatto <matheusfinatto2@gmail.com>
2023-12-15 16:33:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,153 @@
+import { Chess } from "chess.js";
+import { useState } from "react";
+import { Chessboard } from "react-chessboard";
+
+export default function PlayRandomMoveEngine() {
+  const [game, setGame] = useState(new Chess());
+  const [currentTimeout, setCurrentTimeout] = useState();
+
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  if (game.game_over() || game.in_draw())
+    return (
+      <>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "5rem",
+          }}
+        >
+          Game over
+          <button
+            onClick={() => setGame(new Chess())}
+            style={{
+              fontSize: "2rem",
+              borderRadius: "0.5rem",
+              backgroundColor: "#282c34",
+              color: "white",
+              padding: "0.5rem 1rem",
+              border: "none",
+              cursor: "pointer",
+              transition: "background-color 0.3s ease",
+              position: "relative",
+              top: 30,
+            }}
+          >
+            Restart
+          </button>
+        </div>
+      </>
+    );
+
+  function makeMove() {
+    const possibleMoves = game.moves();
+
+    // exit if the game is over
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+      return;
+    //change this random index for the correct input
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+
+    safeGameMutate((game) => {
+      game.move(possibleMoves[randomIndex]);
+    });
+  }
+
+  function onDrop(sourceSquare, targetSquare, piece) {
+    const gameCopy = { ...game };
+    const move = gameCopy.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: piece[1].toLowerCase() ?? "q",
+    });
+    setGame(gameCopy);
+
+    // illegal move
+    if (move === null) return false;
+
+    // store timeout so it can be cleared on undo/reset so computer doesn't execute move
+    const newTimeout = setTimeout(makeMove, 200);
+    setCurrentTimeout(newTimeout);
+    return true;
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "2rem",
+      }}
+    >
+      <div
+        style={{
+          width: "95%",
+          height: "95%",
+          maxHeight: "1000px",
+          maxWidth: "1000px",
+        }}
+      >
+        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+        <br />
+        <div
+          style={{
+            textAlign: "center",
+            display: "flex",
+            gap: "1rem",
+            justifyContent: "center",
+          }}
+        >
+          <button
+            onClick={() => setGame(new Chess())}
+            style={{
+              fontSize: "2rem",
+              borderRadius: "0.5rem",
+              backgroundColor: "#282c34",
+              color: "white",
+              padding: "0.5rem 1rem",
+              border: "none",
+              cursor: "pointer",
+              transition: "background-color 0.3s ease",
+            }}
+          >
+            Restart
+          </button>
+          <button
+            onClick={() => {
+              safeGameMutate((game) => {
+                game.undo();
+                game.undo();
+              });
+              clearTimeout(currentTimeout);
+            }}
+            style={{
+              fontSize: "2rem",
+              borderRadius: "0.5rem",
+              backgroundColor: "#282c34",
+              color: "white",
+              padding: "0.5rem 1rem",
+              border: "none",
+              cursor: "pointer",
+              transition: "background-color 0.3s ease",
+            }}
+          >
+            {" "}
+            Undo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
